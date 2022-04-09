0c04ff309b2b44e108da63089f7dcca94de90028
Buvanesh Kumar <linuxsbk@gmail.com>
2022-04-10 01:51:10 +0530

    Create app codebase

---
+++ application/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def simple_app():
+    return "<p>This is a simple web app!<p>"
+
+@app.route("/version")
+def ver():
+    return "<p>Version: 1.0.0-alpha</p>"
